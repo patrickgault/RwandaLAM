24a1164f5aa598976194df8f3796f28ead306519
flaneuse <laura.d.hughes@gmail.com>
2016-06-10 14:45:41 -0400

    basic structure

--- R/02_RW_cleanDHS_hh.R
+++ R/02_RW_cleanDHS_hh.R
@@ -2,3 +2,15 @@
 # Laura Hughes, USAID GeoCenter, lhughes@usaid.gov
 # 10 June 2016
 # (c) 2016 via MIT License
+
+
+# basic info --------------------------------------------------------------
+
+hh_clean = hh %>% 
+  select(hhid,
+         cluster_id = hv001)
+
+
+# WASH indicators ---------------------------------------------------------
+# @cjolley: want to give it a go?
+hh_clean = hh_clean
