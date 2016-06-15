9a5b4ccc3ff5fb9d7308027b3038db128d0306b9
Craig Jolley <jolleycraig@gmail.com>
2016-06-15 13:40:35 -0400

    Load shapefile for drawing admin2 choropleths.

--- R/03_RW_cleanDHS_geo.R
+++ R/03_RW_cleanDHS_geo.R
@@ -20,3 +20,14 @@ geo_clean = geo %>%
   mutate(rural = ifelse(rural == 'R', 1, # recode rural category to be binary (1 if rural, 0 if urban)
                         ifelse(rural == 'U', 0,
                                NA)))
+
+# load shapefile -----------------------------------------------
+# Note: shapefiles came from: http://www.gadm.org/download
+# I also put them on the google drive: 
+# https://drive.google.com/open?id=0B0cwL9QVH9UKUFpOQ3lBZkR5Snc
+
+rwanda <- readOGR(dsn='GIS/GADM_adm2',layer='RWA_adm2')
+rwanda@data$id = rwanda@data$ID_2
+rwanda.points <- fortify(rwanda,region='ID_2')
+rwanda.adm2 <- join(rwanda.points,rwanda@data,by='id')
+rm(rwanda,rwanda.points)
