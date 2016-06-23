4eb0804f0857e6bfd3963af6caf82505ff0f3343
Craig Jolley <jolleycraig@gmail.com>
2016-06-23 17:31:13 -0400

    Add asset affordability calculations for 2010.

--- R/Digital.R
+++ R/Digital.R
@@ -64,9 +64,9 @@ assets <- hh_all %>%
     earth_floor = as.numeric(hv213 < 21),
     improved_floor = as.numeric(hv213 > 20),
     # wall materials
-    simple_walls = as.numeric(hv214 < 23),
+    bamboo_mud_walls = as.numeric(hv214 == 21 & hv214==27),
     adobe_walls = as.numeric(hv214 == 23),
-    nice_walls = as.numeric(hv214 > 23),
+    covered_adobe_walls = as.numeric(hv214 == 35),
     # roof materials
     simple_roof = as.numeric(hv215 < 31),
     metal_roof = as.numeric(hv215 == 31),
@@ -145,8 +145,82 @@ ggplot(good_assets,aes(x=x,y=w50,label=label)) +
         axis.text.x=element_blank(),
         axis.ticks=element_blank())
 
-# Is there a difference between urban and rural households?
+# What has changed since 2010?
+library(haven)
+hh2010 <- read_dta('Datain/RW_2010_DHS/rwhr61dt/RWHR61FL.DTA')
+hh2010_labels = pullAttributes(hh2010) %>% 
+  mutate(module = 'hh', rowNum = row_number())
+assets2010 <- hh2010 %>% 
+  select(hv205:hv212,hv213:hv215,hv221,hv227,hv242:hv244,hv246,hv247,
+         sh110g,sh118f,hv271) %>%
+  mutate(
+    # fix NAs
+    hv206 = ifelse(hv206==9,NA,hv206),
+    hv207 = ifelse(hv207==9,NA,hv207),
+    hv208 = ifelse(hv208==9,NA,hv208),
+    hv209 = ifelse(hv209==9,NA,hv209),
+    hv210 = ifelse(hv210==9,NA,hv210),
+    hv211 = ifelse(hv211==9,NA,hv211),
+    hv212 = ifelse(hv212==9,NA,hv212),
+    hv221 = ifelse(hv221==9,NA,hv221),
+    hv227 = ifelse(hv227==9,NA,hv227),
+    hv242 = ifelse(hv242==9,NA,hv242),
+    hv243a = ifelse(hv243a==9,NA,hv243a),
+    hv243b = ifelse(hv243b==9,NA,hv243b),
+    hv243c = ifelse(hv243c==9,NA,hv243c),
+    hv243d = ifelse(hv243d==9,NA,hv243d),
+    hv247 = ifelse(hv247==9,NA,hv247),
+    sh110g = ifelse(sh110g==9,NA,sh110g),
+    sh118f = ifelse(sh118f==9,NA,sh118f),
+    # single out the most popular types of toilets
+    pit_toilet = as.numeric(hv205 > 19 & hv205 < 24),
+    no_toilet = as.numeric(hv205 == 31),
+    flush_toilet = as.numeric(hv205 < 16),
+    # floor materials
+    earth_floor = as.numeric(hv213 < 21),
+    improved_floor = as.numeric(hv213 > 20),
+    # wall materials
+    bamboo_mud_walls = as.numeric(hv214 == 21 & hv214==27),
+    adobe_walls = as.numeric(hv214 == 23),
+    covered_adobe_walls = as.numeric(hv214 == 35),
+    # roof materials
+    simple_roof = as.numeric(hv215 < 31),
+    metal_roof = as.numeric(hv215 == 31),
+    nice_roof = as.numeric(hv215 > 31)
+  ) %>%
+  rename(wealth=hv271) %>%
+  select(hv206:hv212,hv221:nice_roof) %>%
+  removeAttributes()
 
+
+test2010 <- ldply(names(select(assets2010,-wealth)),function(n) {
+  label <- n
+  if (n %in% hh2010_labels$var) {
+    label <- hh2010_labels[hh2010_labels$var==n,'varDescrip']
+  }
+  f <- paste(n,' ~ wealth',collapse='') %>% as.formula()
+  s <- glm(f,family=binomial(link='logit'),data=assets2010) %>% summary()
+  w50 <- -s$coefficients[1,1]/s$coefficients[2,1]/1e5
+  in_range <- w50 > min(assets2010$wealth) & w50 < max(assets2010$wealth)
+  haves <- assets2010[assets2010[,n]==1,'wealth']
+  have_nots <- assets2010[assets2010[,n]==0,'wealth']
+  pos <- mean(haves,na.rm=TRUE) > mean(have_nots,na.rm=TRUE)
+  data.frame(n=n,label=label,in_range=in_range,pos=pos,w50=w50)
+})
+good_assets2010 <- test2010[test2010$in_range & test2010$pos,c('n','label','w50')]
+good_assets2010 <- good_assets2010[order(good_assets2010$w50),]
+good_assets2010
+
+good_assets <- rename(good_assets,w50_2015=w50)
+good_assets2010 <- rename(good_assets2010,w50_2010=w50)
+compare_assets <- join(good_assets,good_assets2010,by='label') %>% select(label,w50_2010,w50_2015) %>%
+  mutate(diff=w50_2015-w50_2010)
+
+# So it looks like most things came down, suggesting that items became 
+# within reach for more people between 2010 and 2015. Mobiles showed
+# a decrease, though not the most dramatic one -- they seem to have
+# switched places with radio as the first electronic device people buy,
+# leading radio to be the only item to show an increase.
 
 # Which household demographics are most predictive of mobile ownership?
 
